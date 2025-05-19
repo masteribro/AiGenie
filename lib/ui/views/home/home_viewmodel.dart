4b8adde5ccadc7fad1e10e533ddbbe5a500c6f54
masteribro <mohammedibrahim075@gmail.com>
2025-05-19 16:47:21 +0100

    ❇️fix: removed al service file

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -4,7 +4,6 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import '../../../app/app.locator.dart';
-import '../../../service/ai_service.dart';
 
 class HomeViewModel extends BaseViewModel {
   final _dialogService = locator<DialogService>();
